e0a6c04eb5435d89b6d853f345de3b98b72140c1
Satish Yadav <monstersatish@live.com>
2021-01-17 23:07:04 +0530

    Add project files.

---
+++ HashGrpc.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using HashGrpc.Protos;
+using System;
+using System.Threading.Tasks;
+using static HashGrpc.Protos.Hash;
+
+namespace HashGrpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HashClient(channel);
+            var response = await client.GetHashAsync(new HashRequest { Input = "Test" });
+            Console.WriteLine($"Hash: {response.Hash}");
+        }
+    }
+}
---
+++ HashGrpc/Services/HashService.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using HashGrpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashGrpc.Services
+{
+    public class HashService : Hash.HashBase
+    {
+        public override Task<HashResponse> GetHash(HashRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HashResponse
+            {
+                Hash = GetSha256Hash(request.Input)
+            });
+        }
+
+        private static string GetSha256Hash(string plainText)
+        {
+            // Create a SHA256 hash from string   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Computing Hash - returns here byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+                // now convert byte array to a string   
+                StringBuilder stringbuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stringbuilder.Append(bytes[i].ToString("x2"));
+                }
+                return stringbuilder.ToString();
+            }
+        }
+    }
+}
